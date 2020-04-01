d799301f3f597a8b761a6e122539cafb3e6f14fd
Mert  Ozkaynak <mertozkaynak@windowslive.com>
2020-04-01 22:03:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -31,7 +31,7 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
  planeteer_calls.find { |call|
- include? valid_calls 
+ .include? valid_calls 
  }
 end
 
